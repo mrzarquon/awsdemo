4d2be6db7fa384c9f84dbbe31d47105e0991c49a
Chris Barker <cbarker@puppetlabs.com>
2015-10-07 13:48:39 -0700

    add multinode example calling linuxnode defined type

---
+++ examples/multiplenodes.pp
@@ -0,0 +1,16 @@
+include awsdemo::params
+
+$public_key = split($ec2_metadata['public-keys']['0']['openssh-key'], ' ')
+$key_name = $public_key[2]
+
+Awsdemo::Linuxnode {
+  pp_department   => $ec2_tags['department'],
+  pp_project      => $ec2_tags['project'],
+  pp_created_by   => $ec2_tags['created_by'],
+  key_name        => $key_name,
+  image_ids       => $awsdemo::params::image_ids,
+  security_groups => ['tse-agents','tse-crossconnect'],
+  subnet          => 'tse-subnet-avza-1',
+}
+
+awsdemo::linuxnode { ['redhat7-01','redhat7-02']: }
--- manifests/linuxnode.pp
+++ manifests/linuxnode.pp
@@ -1,24 +1,25 @@
+# this defines a wrapper class, really to capture the params so they can be
+# evaled by the templating engine
 define awsdemo::linuxnode (
-  $nodename = $title,
-  $availability_zone = $awsdemo::params::availability_zone,
-  $image_id = $awsdemo::params::redhat7,
-  $region = $awsdemo::params::region,
-  $instance_type = 'm4.medium',
-  $security_groups = $awsdemo::params::security_groups,
-  $subnet = $awsdemo::params::subnet,
-  $pe_version_string = $awsdemo::params::pe_version_string,
   $pp_department,
   $pp_project,
   $pp_created_by,
   $key_name,
-  $pe_master_hostname,
+  $image_ids,
+  $security_groups,
+  $subnet,
+  $pe_master_hostname = $::ec2_metadata['local-hostname'],
+  $nodename           = $title,
+  $availability_zone  = $::ec2_metadata['placement']['availability-zone'],
+  $region             = $::ec2_region,
+  $instance_type      = 'm4.medium',
 ) {
   include awsdemo::params
 
   ec2_instance { $nodename:
     ensure            => 'running',
     availability_zone => $availability_zone,
-    image_id          => $image_id,
+    image_id          => $image_ids[$ec2_region]['redhat7'],
     instance_type     => $instance_type,
     key_name          => $key_name,
     region            => $region,
@@ -29,7 +30,7 @@ define awsdemo::linuxnode (
       'project'       => $pp_project,
       'created_by'    => $pp_created_by,
     },
-    user_data         => template('tse_awsnodes/linux.erb'),
+    user_data         => template('awsdemo/linux.erb'),
   }
 
 }
