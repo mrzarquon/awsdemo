971256df2afe036be380ef6135af67bd13270f95
Chris Barker <cbarker@puppetlabs.com>
2015-10-07 14:12:38 -0700

    add pp_role example to multi node

--- examples/trusted_rule.sh
+++ examples/trusted_rule.sh
@@ -3,13 +3,14 @@ PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
 curl -X POST -H 'Content-Type: application/json' \
 -d \
 '{
-    "name": "TrustedFact",
+    "name": "WebServer",
     "environment": "production",
     "parent": "00000000-0000-4000-8000-000000000000",
     "rule": [
         "=",
         [
             "trusted",
+            "extensions",
             "pp_role"
         ],
         "webserver"
